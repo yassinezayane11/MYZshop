3041f0890686de6272f6e9469bd0421196480e83
yassinezayane11 <yassinezayane11@gmail.com>
2026-04-29 14:29:43 +0100

    tt

--- server/index.js
+++ server/index.js
@@ -7,9 +7,14 @@ const mongoose = require('mongoose');
 const app = express();
 
 // Middleware
+const cors = require('cors');
+
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true,
+  origin: [
+    'http://localhost:5173',
+    'https://myzshop.vercel.app'
+  ],
+  credentials: true
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
